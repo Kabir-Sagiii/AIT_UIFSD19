11350bbedd9eeedfd8c03ce95d060b480c257bb3
Kabir <sagithedeveloper@gmail.com>
2025-08-21 10:29:46 +0530

    Added the code

---
+++ 05_ReactJS/S53_Vite_Tool/firstapp/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>First App Component</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt
+        quaerat necessitatibus omnis eligendi mollitia hic repudiandae placeat?
+        Asperiores earum laboriosam quidem iure, facilis obcaecati dolor. Ex,
+        fuga. Quisquam excepturi sunt libero! Consectetur, id nesciunt quisquam
+        adipisci dolorem magni modi saepe doloremque, dicta nihil natus
+        repellendus, vel corrupti voluptatem aperiam rem consequuntur eaque
+        nostrum quas ipsum cupiditate dolores illum expedita quam. Voluptatibus
+        aspernatur nihil eligendi quae error provident optio ratione nobis quos
+        necessitatibus, unde facere autem facilis deleniti quis cumque!
+        Accusamus placeat facilis quos eligendi. Excepturi unde nostrum aut ex
+        consequatur aliquid, praesentium eaque labore nesciunt vero id,
+        inventore reprehenderit facilis.
+      </p>
+    </div>
+  );
+}
+
+export default App;
