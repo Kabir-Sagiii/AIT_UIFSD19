6e8bc894927608a6a49503da7d93e23497dd2ec5
Kabir <sagithedeveloper@gmail.com>
2025-08-22 10:30:58 +0530

    added the code

---
+++ 05_ReactJS/S54_Components/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
