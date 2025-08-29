65cc220cb35b6c5d439ffee16e78852d98b52b83
Kabir <sagithedeveloper@gmail.com>
2025-08-29 10:33:05 +0530

    Added the code

---
+++ 05_ReactJS/S57_State_Arrays/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Counter from "./components/counter/Counter";
+import TechList from "./components/list/TechList";
+
+function App() {
+  return (
+    <div className="app">
+      <TechList />
+    </div>
+  );
+}
+
+export default App;
